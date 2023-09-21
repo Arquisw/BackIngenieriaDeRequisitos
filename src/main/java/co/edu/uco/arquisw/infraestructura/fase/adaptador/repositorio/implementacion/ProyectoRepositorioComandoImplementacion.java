96b1eea7b579cfb50c0106ea1bab3800499a4665
CEIBA\esteban.colorado <estecogn@suramericana.com.co>
2023-09-21 14:34:49 -0500

    Se agrega bean faltante

--- src/main/java/co/edu/uco/arquisw/infraestructura/fase/adaptador/repositorio/implementacion/ProyectoRepositorioComandoImplementacion.java
+++ src/main/java/co/edu/uco/arquisw/infraestructura/fase/adaptador/repositorio/implementacion/ProyectoRepositorioComandoImplementacion.java
@@ -5,7 +5,9 @@ import co.edu.uco.arquisw.dominio.fase.puerto.comando.ProyectoRepositorioComando
 import co.edu.uco.arquisw.infraestructura.fase.adaptador.mapeador.proyecto.EstadoProyectoMapeador;
 import co.edu.uco.arquisw.infraestructura.fase.adaptador.repositorio.jpa.proyecto.ProyectoDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ProyectoRepositorioComandoImplementacion implements ProyectoRepositorioComando {
     @Autowired
     ProyectoDAO proyectoDAO;
