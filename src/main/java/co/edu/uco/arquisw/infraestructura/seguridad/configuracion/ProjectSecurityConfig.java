b22a2d98ae8df671c902cda63a8eab165159e103
CEIBA\esteban.colorado <estecogn@suramericana.com.co>
2023-10-16 15:52:36 -0500

    se elimina import sin usar

--- src/main/java/co/edu/uco/arquisw/infraestructura/seguridad/configuracion/ProjectSecurityConfig.java
+++ src/main/java/co/edu/uco/arquisw/infraestructura/seguridad/configuracion/ProjectSecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
